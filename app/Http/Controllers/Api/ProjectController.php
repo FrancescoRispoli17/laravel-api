d569c6a95cc6a5c5d063e7cf56c4ddbaa2422a96
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-08-27 18:52:50 +0200

    [UPD] show detail function for api ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,10 +9,28 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with('technologies','type')->paginate(16);
+        $projects = Project::with('type')->paginate(16);
         return response()->json([
             'status' => true,
             'results' =>$projects
         ]);
+    }
+
+    public function show(string $slug){
+
+        $project = Project::where('slug',$slug)->with('technologies','type')->first();
+        if($project){
+            return response()->json([
+                'status' => true,
+                'results' =>$project
+            ]);
+        }
+        else{
+            return response()->json([
+                'status' => false,
+                'results' =>null
+            ],404);
+        }
+        return response()->json($project);
     }
 }
